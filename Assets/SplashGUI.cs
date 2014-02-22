be5441efa131f5036504ed3bdcb668dfef2d170f
Bryan Klimt <bklimt@gmail.com>
2014-02-22 01:03:27 -0800

    Add a splash screen.

---
+++ Assets/SplashGUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashGUI : MonoBehaviour {
+
+	public GUISkin defaultSkin;
+
+	void Start() {
+	}
+	
+	void Update() {
+	}
+
+	void OnGUI() {
+		GameState state = GameState.Instance;
+
+		GUI.skin = defaultSkin;
+		GUI.Label(new Rect(0, 0, Screen.width, Screen.height / 2), "Land the Eagle");
+		Rect button1Rect = GameState.GetGUIButtonRect(0);
+		if (GUI.Button(button1Rect, "Start")) {
+			state.RestartLevel();
+		}
+	}
+
+}
